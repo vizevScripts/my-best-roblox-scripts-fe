473b7845d7e6a9a4e6ba74f587beeae3bcd5e628
vizevScripts <162636218+vizevScripts@users.noreply.github.com>
2025-02-13 19:34:44 +0500

    Update Baer Script Roblox v0.1.lua

--- Baer Script Roblox v0.1.lua
+++ Baer Script Roblox v0.1.lua
@@ -6,4 +6,4 @@
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vizevScripts/my-best-roblox-scripts-fe/refs/heads/main/Settings-Scripts/Destroy-trollZ-in-Baer.lua"))()
 
-loadstring(game:HttpGet
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vizevScripts/my-best-roblox-scripts-fe/refs/heads/main/Settings-Scripts/Baer%20Script%20Roblox%20v0.1.lua"))()
