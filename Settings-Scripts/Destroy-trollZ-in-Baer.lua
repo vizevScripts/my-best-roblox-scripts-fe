30b38bd699f983d1d7f369874f0fcb4e0dfb311c
vizevScripts <162636218+vizevScripts@users.noreply.github.com>
2025-02-13 19:30:36 +0500

    Create Destroy-trollZ-in-Baer.lua

---
+++ Settings-Scripts/Destroy-trollZ-in-Baer.lua
@@ -0,0 +1,11 @@
+local ItzNotFunnyBr0____________________________________
+local Finally_________this_Tr0llllllllllllllllllllllllllllll______________ = false
+
+repeat wait()
+	for _, Trollz in pairs(game:GetChildren()) do
+		if Trollz:IsA('Model') and Trollz.Name == "le epic troll" then
+			Finally_________this_Tr0llllllllllllllllllllllllllllll______________ = true
+			Trollz:Destroy() -- Here we got! MUHAHAHAHAHAHA XD
+		end
+	end
+until Finally_________this_Tr0llllllllllllllllllllllllllllll______________
