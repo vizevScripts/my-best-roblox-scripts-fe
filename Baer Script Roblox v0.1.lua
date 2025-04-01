35496d084474d81384a68029aef1b39a81b3b5b3
vizevScripts <162636218+vizevScripts@users.noreply.github.com>
2025-04-01 19:08:15 +0300

    Delete Baer Script Roblox v0.1.lua

--- Baer Script Roblox v0.1.lua
+++
@@ -1,9 +0,0 @@
-[[
-
- -Coded by @mrNugget34284320 (Vizev)
-
-]]
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vizevScripts/my-best-roblox-scripts-fe/refs/heads/main/Settings-Scripts/Destroy-trollZ-in-Baer.lua"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vizevScripts/my-best-roblox-scripts-fe/refs/heads/main/Settings-Scripts/Baer%20Script%20Roblox%20v0.1.lua"))()
