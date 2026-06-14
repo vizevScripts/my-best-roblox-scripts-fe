d387d8f534d1857c73745f62167e956479089bb1
vizevScripts <162636218+vizevScripts@users.noreply.github.com>
2026-06-14 15:54:39 +0300

    Update Notification Module.lua

--- Notification Module.lua
+++ Notification Module.lua
@@ -2,19 +2,12 @@
 	Module by @mrNugget34284320 (in Roblox)
 			--Functions:--
 	--Notification:CreateNotification(string Title, string Text, Duration)
+	--
 				------------
 		--I hate people who steals my scripts and edits credits on the script.
 ]]
 
-print([[
-	UPDATED! v0.2
-	Made by mrNugget34284320
-]])
-
-local CanPrintGood = false
-local Alone = false
-
-if not Start then Start = not true end
+local Start = false
 
 local Notification = {}
 
@@ -29,89 +22,101 @@ end
 
 ThisNotificationGui.Name = Player.Name .. "'s Notification Gui!"
 
-local function GetMaxPlayers()
-	if game:GetService'Players'.MaxPlayers == 1 then
-		warn('Are you alone?')
-		Alone = true
-		CanPrintGood = false
-	else
-		print('Good!')
-		Alone = false
-		CanPrintGood = true
-	end
-end
-
 local UICorner = Instance.new("UICorner")
 UICorner.CornerRadius = UDim.new(0, 26)
-
-local function ShutdownGame()
-	game.shutdown(game)
-end
 
 local NotificationGui = ThisNotificationGui
 local NotificationFrame
 local NotificationTitle
 local NotificationText
 
+local FolderOfSounds = Instance.new("Folder", ThisNotificationGui)
+FolderOfSounds.Name = "Folder of Sounds"
+local WarningSound = Instance.new("Sound", FolderOfSounds)
+WarningSound.SoundId = "rbxassetid://139761494230828"
+WarningSound.Volume = .5
+local NotificationSound = Instance.new("Sound", FolderOfSounds)
+NotificationSound.SoundId = "rbxassetid://131661992591924"
+NotificationSound.Volume = 5
+
 function Notification:CreateNotification(Title: string?, Text: string?, Duration)
-	local Frame = Instance.new('Frame', ThisNotificationGui)
-	Frame.Name = Player.Name.."'s Notification"
-	Frame.Size = UDim2.new(0, 454,0, 88)
-	Frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-	Frame.Position = UDim2.new(-0.607, 0,0.008, 0)
-
-	local text = Instance.new("TextLabel", Frame)
-	text.Name = "Text"
-	text.BackgroundTransparency = 1
-	text.Position = UDim2.new(0.026, 0,0.496, 0)
-	text.TextScaled = not false
-	text.Size = UDim2.new(0, 419,0, 38)
-	text.TextColor3 = Color3.fromRGB(0, 0, 0)
-	text.Text = "Text"
-
-	local title = Instance.new("TextLabel", Frame)
-	title.Name = "Title"
-	title.BackgroundTransparency = 1
-	title.Position = UDim2.new(0.026, 0,0.064, 0)
-	title.TextScaled = not false
-	title.Size = UDim2.new(0, 419,0, 38)
-	title.TextColor3 = Color3.fromRGB(0, 0, 0)
-	title.Text = "Title"
+	local Notice = {}
+	local SoundType = "Warning"
 	
-	Start = true
+	function Notice:StartNotification()
+		local Frame = Instance.new('Frame', ThisNotificationGui)
+		Frame.Name = Player.Name.."'s Notification"
+		Frame.Size = UDim2.new(0, 454,0, 88)
+		Frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+		Frame.Position = UDim2.new(-0.607, 0,0.008, 0)
 
-	NotificationFrame = Frame
-	NotificationText = text
-	NotificationTitle = title
+		local text = Instance.new("TextLabel", Frame)
+		text.Name = "Text"
+		text.BackgroundTransparency = 1
+		text.Position = UDim2.new(0.026, 0,0.496, 0)
+		text.TextScaled = true
+		text.Size = UDim2.new(0, 419,0, 38)
+		text.TextColor3 = Color3.fromRGB(0, 0, 0)
+		text.Text = "Text"
 
-	UICorner.Parent = Frame
+		local title = Instance.new("TextLabel", Frame)
+		title.Name = "Title"
+		title.BackgroundTransparency = 1
+		title.Position = UDim2.new(0.026, 0,0.064, 0)
+		title.TextScaled = true
+		title.Size = UDim2.new(0, 419,0, 38)
+		title.TextColor3 = Color3.fromRGB(0, 0, 0)
+		title.Text = "Title"
+		
+		if SoundType == "Warning" then
+			WarningSound:Play()
+		elseif SoundType == "Notification" then
+			NotificationSound:Play()
+		end
 
-	if Start then
-		NotificationFrame:TweenPosition(UDim2.new(-0.071, 0,0.012, 0)) 
+		Start = true
+
+		NotificationFrame = Frame
+		NotificationText = text
+		NotificationTitle = title
+
+		UICorner.Parent = Frame
+
+		if Start then
+			NotificationFrame:TweenPosition(UDim2.new(-0.071, 0,0.012, 0)) 
+		end
+
+		if not Duration then 
+			Duration = 5 
+			warn("You didn't set a duration")
+		end
+
+		if Duration < 0.01 then NotificationGui:Destroy() end
+
+		NotificationTitle.Text = Title
+		NotificationText.Text = Text
+
+		wait(Duration)
+
+		NotificationFrame:TweenPosition(UDim2.new(-0.607, 0,0.008, 0))
+
+		Start = false
 	end
-
-	if not Duration then 
-		Duration = 5 
-		warn("You didn't set a duration")
+	
+	function Notice:SetNotificationSound(NoticeSoundType)
+		if NoticeSoundType == "Warning" then
+			SoundType = "Warning"
+		elseif NoticeSoundType == "Notification" then
+			SoundType = "Notification"
+		end
 	end
-
-	if Duration < 0.01 then NotificationGui:Destroy() end
-
-	NotificationTitle.Text = Title
-	NotificationText.Text = Text
-
-	wait(Duration)
-
-	NotificationFrame:TweenPosition(UDim2.new(-0.607, 0,0.008, 0))
+	
+	return Notice
 end
 
-spawn(function()
-	repeat
-		wait(0.3)
-		GetMaxPlayers()
-		if Alone == true then Alone = false break end
-		if CanPrintGood == true then CanPrintGood = false break end
-	until wait()
-end)
+print([[
+	UPDATED! v0.3
+	Made by mrNugget34284320
+]])
 
 return Notification
