033ebee221b95cedceb51adb5247d404f634cd10
vizevScripts <162636218+vizevScripts@users.noreply.github.com>
2026-06-20 19:30:34 +0300

    Update Random State FE.lua

--- Random State FE.lua
+++ Random State FE.lua
@@ -3,10 +3,26 @@ NOTICE: MADE BY ISLAM (@mrNugget34284320)
 ALL SCRIPTS ARE MINE!
 		-FE Random State-
 ]]
-local Humanoid = game.Players.LocalPlayer.Character.Humanoid
 
-local Enumes = Enum.HumanoidStateType:GetEnumItems()
+local Looped = false
 
-local RandomState = Enumes[math.random(1, #Enumes)]
+local Player = game:GetService("Players").LocalPlayer
+local PlayerCharacter = Player.Character or Player.CharacterAdded:Wait()
+local CharacterHumanoid = PlayerCharacter:FindFirstChildOfClass("Humanoid") 
 
-Humanoid:ChangeState(RandomState)
+local function PlayRandomState()
+	if Player and PlayerCharacter and CharacterHumanoid and CharacterHumanoid.Health > 0 then
+		local Enumes = Enum.HumanoidStateType:GetEnumItems()
+		local RandomState = Enumes[math.random(1, #Enumes)]
+		CharacterHumanoid:ChangeState(RandomState)
+	end
+end
+
+while wait() do
+	if Looped then
+		PlayRandomState()
+	else
+		PlayRandomState()
+		break
+	end
+end
