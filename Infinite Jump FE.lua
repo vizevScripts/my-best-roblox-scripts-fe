1062d3287886bc9667f19dc89984807b6c55dcd5
vizevScripts <162636218+vizevScripts@users.noreply.github.com>
2026-06-20 19:29:58 +0300

    Update Infinite Jump FE.lua

--- Infinite Jump FE.lua
+++ Infinite Jump FE.lua
@@ -5,15 +5,19 @@
 
 --Notice: Everyone can see the hacks, because it's FE script!
 
-local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+local Player = game:GetService("Players").LocalPlayer
+local PlayerCharacter = Player.Character or Player.CharacterAdded:Wait()
+local CharacterHumanoid = PlayerCharacter:FindFirstChildOfClass("Humanoid") 
 
 local JumpCount = 0
 
 while wait() do
-	if Humanoid.Jump == true then
-		JumpCount = JumpCount + 1
-		if JumpCount > 0 then
-			Humanoid.HipHeight = Humanoid.HipHeight + 1
-		end	
+	if Player and PlayerCharacter and CharacterHumanoid and CharacterHumanoid.Health > 0 then
+		if CharacterHumanoid.Jump == true then
+			JumpCount = JumpCount + 1
+			if JumpCount > 0 then
+				Humanoid.HipHeight = CharacterHumanoid.HipHeight + 1
+			end	
+		end
 	end
 end
